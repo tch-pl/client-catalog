63344aadc2aa2457836b057eadde9a181f051e3e
unknown <user@compton.(none)>
2013-05-25 12:57:16 +0200

    configured  home page

--- client-catalog-controller/src/main/java/tch/code/clientcatalog/controller/springmvc/ClientCatalogController.java
+++ client-catalog-controller/src/main/java/tch/code/clientcatalog/controller/springmvc/ClientCatalogController.java
@@ -9,8 +9,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
+import org.springframework.web.bind.annotation.RequestParam;
 import tch.code.clientcatalog.core.logic.ClientService;
-import tch.code.clientcatalog.core.service.data.dao.ClientDAO;
 import tch.code.clientcatalog.core.service.data.model.ClientDTO;
 
 /**
@@ -19,21 +19,46 @@ import tch.code.clientcatalog.core.service.data.model.ClientDTO;
  */
 @Controller(value = "ClientCatalogController")
 public class ClientCatalogController {
-    private final int PAGE_SIZE = 50;
+    private final int PAGE_SIZE = 1;
+    private final int DEFAULT_PAGE_INDEX = 0;
+    
+    private final String CLIENT_LIST_PATH = "/client_list.html";
     
     private ClientService clientService;
 
-    @RequestMapping(method=GET, value="/client-catalog")
-    public String redirect(Map<String, Object> model) {
-        return "redirect:/client-catalog/client-list.html";
+    @RequestMapping({"/","/home"})
+    public String home(Map<String, Object> model) {
+        return "redirect:"+CLIENT_LIST_PATH;
     }
     
-    @RequestMapping(value="/client_list.html", method=GET)
-    public String listClients(Map<String, Object> model) {
-        // always get first page
-        List<ClientDTO> clientsToView = clientService.pageClients(PAGE_SIZE, clientService.findAllClients()).get(0);
+    @RequestMapping(method=GET, value="/client-catalog")
+    public String redirect(Map<String, Object> model) {
+        return "redirect:"+CLIENT_LIST_PATH;
+    }
+    
+    @RequestMapping(method=GET, value="/index.html")
+    public String mainPage(Map<String, Object> model) {
+        return "redirect:"+CLIENT_LIST_PATH;
+    }
+    
+    @RequestMapping(value=CLIENT_LIST_PATH, method=GET)
+    public String listClients(Map<String, Object> model, @RequestParam(value="page",required=false) Integer page, @RequestParam(value="mode",required=false) String mode) {               
+        Map<Integer, List> paged_client_data = clientService.pageClients(PAGE_SIZE, clientService.findAllClients());
+        Integer current_index = page != null && page < paged_client_data.size() ? page : DEFAULT_PAGE_INDEX;
+        List<ClientDTO> clientsToView = paged_client_data.get(current_index);
         model.put("client_list", clientsToView);
+        model.put("page_count", paged_client_data.size());
+        model.put("current_index", current_index);
+        if ("ajax".equals(mode)) {
+            return "client_list_table";
+        }
         return "home";
+    }
+
+    @RequestMapping(value="/ajax"+CLIENT_LIST_PATH, method=GET)
+    public String listClientsAjax(Map<String, Object> model, @RequestParam(value="page",required=false) Integer page, @RequestParam(value="mode",required=false) String mode) {               
+        this.listClients(model, page, mode);
+        return "client_list_table";        
     }
     
     @RequestMapping(value="/client_add_form.html", method=GET)
