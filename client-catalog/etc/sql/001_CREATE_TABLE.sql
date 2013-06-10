821113139ec799fb2b9d193303e870334acc4e4f
unknown <user@compton.(none)>
2013-06-10 11:50:22 +0200

    sql change

--- client-catalog/etc/sql/001_CREATE_TABLE.sql
+++ client-catalog/etc/sql/001_CREATE_TABLE.sql
@@ -18,15 +18,15 @@ create table client (client_id INT NOT NULL GENERATED ALWAYS AS IDENTITY(START W
 -- create table client (ID INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT CLIENT_PK PRIMARY KEY,
 --                         client_type INT NOT NULL,
 --                         description VARCHAR(200));
-create table client_company (ID INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT CLIENT_COMPANY_PK PRIMARY KEY,                        
-                        company_name VARCHAR(200),
-                         client_id INT NOT NULL,
-                         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE RESTRICT);
- 
- create table client_private_person (ID INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT CLIENT_PRIVATE_PERSON_PK PRIMARY KEY,                        
-                         first_name VARCHAR(100),
-                         last_name VARCHAR(100),
-                         client_id INT NOT NULL,
-                         FOREIGN KEY (client_id) REFERENCES client (client_id) ON DELETE CASCADE ON UPDATE RESTRICT);
+--create table client_company (ID INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT CLIENT_COMPANY_PK PRIMARY KEY,                        
+--                        company_name VARCHAR(200),
+--                         client_id INT NOT NULL,
+--                         FOREIGN KEY (client_id) REFERENCES client (id));
+-- 
+-- create table client_private_person (ID INT NOT NULL GENERATED ALWAYS AS IDENTITY CONSTRAINT CLIENT_PRIVATE_PERSON_PK PRIMARY KEY,                        
+--                         first_name VARCHAR(100),
+--                         last_name VARCHAR(100),
+--                         client_id INT NOT NULL,
+--                         FOREIGN KEY (client_id) REFERENCES client (id));
 
 CREATE SEQUENCE client_sequence AS BIGINT START WITH 3000000000;
